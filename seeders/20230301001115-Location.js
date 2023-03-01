2754a1e8e4305d309f2f380bbf98073773cc8a12
Agustin Muniz <agus.muniz1997@gmail.com>
2023-02-28 21:37:26 -0300

    Seeds Issue

--- seeders/20230301001115-Location.js
+++ seeders/20230301001115-Location.js
@@ -3,7 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('Acquirer', [
+    await queryInterface.bulkInsert('Location', [
       { name: 'STOCK-BULEVAR', createdAt: new Date(), updatedAt: new Date() },
       { name: 'LEOPOLDO GROSS', createdAt: new Date(), updatedAt: new Date() },
       { name: 'LABORATORIO', createdAt: new Date(), updatedAt: new Date() },
